bca3b9fef6f2b241bd2e17586fd2bc1c2881243b
Anirudh Madhavan <anirudh@homigo.in>
2018-08-13 12:42:37 +0530

    Add : prop-types -> flow

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,5 +1,6 @@
+// @flow
+
 import React from 'react';
-import { string } from 'prop-types';
 import styled from 'styled-components';
 
 const color = ' #333'; // Just to show variables can be used in tagged temlate literal while defining styles
@@ -20,7 +21,7 @@ const Image = styled.img`
 	margin-right: 10px;
 `;
 
-const ShowCard = props => (
+const ShowCard = (props: { poster: string, title: string, year: string, description: string }) => (
 	<Wrapper>
 		<Image alt={`${props.title} Show Poster`} src={`/public/img/posters/${props.poster}`} />
 		<div>
@@ -31,12 +32,5 @@ const ShowCard = props => (
 		</div>
 	</Wrapper>
 );
-
-ShowCard.propTypes = {
-	poster: string.isRequired,
-	title: string.isRequired,
-	year: string.isRequired,
-	description: string.isRequired
-};
 
 export default ShowCard;
