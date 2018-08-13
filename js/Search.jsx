f0e2a53d873202f606c915c2a461251e7a9ef80f
Anirudh Madhavan <anirudh@homigo.in>
2018-08-13 21:12:29 +0530

    Add : Flow typed finalllyyy!!!

--- js/Search.jsx
+++ js/Search.jsx
@@ -5,53 +5,53 @@ import ShowCard from './ShowCard';
 import preload from '../data.json';
 
 class Search extends Component {
-	// Older method to construct, assign state and bind mehtods with the context
-	// constructor(props) {
-	// 	super(props);
+  // Older method to construct, assign state and bind mehtods with the context
+  // constructor(props) {
+  //  super(props);
 
-	// 	this.state = {
-	// 		searchTerm: 'this is some sort of debug statement'
-	// 	};
+  //  this.state = {
+  //    searchTerm: 'this is some sort of debug statement'
+  //  };
 
-	// 	this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
-	// }
+  //  this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  // }
 
-	// Transform class properties still in 2
+  // Transform class properties still in 2
 
-	// Flow is gonna type your state according to the initial values passed here below
-	state = {
-		searchTerm: ''
-	};
-	handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
-		// event is a synthetic event not exactly a DOM event. For performance. It's an event but not the one you think!
-		this.setState({ searchTerm: event.target.value });
-	};
-	render() {
-		return (
-			<div className="search">
-				<header>
-					<h1>
-						<span>mFlix</span>
-					</h1>
-					<input
-						onChange={this.handleSearchTermChange}
-						type="text"
-						value={this.state.searchTerm}
-						placeholder="Search"
-					/>
-				</header>
-				<div>
-					{preload.shows
-						.filter(
-							show => `${show.title} ${show.desciption}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
-						)
-						.map(show => (
-							<ShowCard key={show.imdbID} {...show} />
-						))}
-				</div>
-			</div>
-		);
-	}
+  // Flow is gonna type your state according to the initial values passed here below
+  state = {
+    searchTerm: ''
+  };
+  handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
+    // event is a synthetic event not exactly a DOM event. For performance. It's an event but not the one you think!
+    this.setState({ searchTerm: event.target.value });
+  };
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>
+            <span>mFlix</span>
+          </h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            type="text"
+            value={this.state.searchTerm}
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              show => `${show.title} ${show.desciption}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => (
+              <ShowCard key={show.imdbID} {...show} />
+            ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Search;
